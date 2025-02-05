951855d0e592ae27193422a58c7bc94bc768a762
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-02-05 18:29:59 -0500

    Fix build

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -10,8 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 const BASE_URL = new URL(
   process.env.NEXT_PUBLIC_BASE_URL ||
-    process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-    ''
+    `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
 )
 
 export const metadata: Metadata = {
