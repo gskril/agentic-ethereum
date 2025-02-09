7be56f5a821f7ae7e2272d6f98eb829f7aad08aa
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-02-08 23:18:16 -0500

    Update winner screen

--- apps/web/src/components/WinnerChosen.tsx
+++ apps/web/src/components/WinnerChosen.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { useEnsName } from 'wagmi'
+import { formatEther } from 'viem'
+import { useEnsAvatar, useEnsName } from 'wagmi'
 
-import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Game } from '@/hooks/useLatestGame'
 import { truncateAddress } from '@/lib/utils'
 
@@ -16,14 +17,36 @@ export function WinnerChosen({ game }: Props) {
     chainId: 1,
   })
 
+  const { data: avatar } = useEnsAvatar({
+    name: name ?? undefined,
+    chainId: 1,
+  })
+
+  const feesPaid = game.entryFee * game.playersCount
+  const fee = feesPaid / 10n // 10% fee
+  const prize = feesPaid - fee
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4 p-4">
       <Card className="w-full max-w-sm shadow-lg">
         <CardHeader>
-          <h1 className="text-center text-2xl font-bold">{game.title}</h1>
+          <CardTitle className="text-center">Previous Game</CardTitle>
         </CardHeader>
-        <CardContent>
-          <p>Winner: {name ?? truncateAddress(game.winner)}</p>
+        <CardContent className="flex flex-col items-center">
+          {avatar && (
+            <img
+              src={`https://ens-api.gregskril.com/avatar/${name}?width=64`}
+              className="mb-4 h-16 w-16 rounded-full"
+            />
+          )}
+
+          <span className="text-xl font-bold">
+            {name ?? truncateAddress(game.winner)}
+          </span>
+
+          <span>won</span>
+
+          <span className="text-xl font-bold">{formatEther(prize)} ETH</span>
         </CardContent>
       </Card>
     </div>
